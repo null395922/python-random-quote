4785a04c216ef62aca642b84248e8216bd00d74d
null395922 <45619797+null395922@users.noreply.github.com>
2021-12-04 16:08:52 +0800

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -4,8 +4,10 @@ def main():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+  last = 13
+  rnd = random.randint(0, last)
 
-  print(quotes[0])
+  print(quotes[rnd])
 
 if __name__== "__main__":
   main()
